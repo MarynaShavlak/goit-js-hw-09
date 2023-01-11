904d75a389adf8e2e51d6e193db1c06b92025b56
MarynaShavlak <111526360+MarynaShavlak@users.noreply.github.com>
2023-01-11 16:50:38 +0000

    Deploying to gh-pages from @ MarynaShavlak/goit-js-hw-09@f193b6b86100e1f16a2a1159558f5773adc2a022 🚀

--- 01-color-switcher.4f05f66d.js
+++
@@ -1,2 +0,0 @@
-startBtn=document.querySelector("button[data-start]"),stopBtn=document.querySelector("button[data-stop]"),bodyEl=document.querySelector("body");let t=null;function e(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}startBtn.addEventListener("click",(function(){bodyEl.style.backgroundColor=e(),startBtn.disabled=!0,stopBtn.disabled=!1,t=setInterval((()=>{bodyEl.style.backgroundColor=e()}),1e3)})),stopBtn.addEventListener("click",(function(){clearInterval(t),startBtn.disabled=!1,stopBtn.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.4f05f66d.js.map
